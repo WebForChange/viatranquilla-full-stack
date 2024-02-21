ad55120bc5a1a02345c445e99b7bd9654cf50c82
Stefan Lange <stefanlange@maeve.de>
2024-02-21 14:44:23 +0100

    implemented provider created login register page

--- frontend/src/contexts/AuthProvider.jsx
+++ frontend/src/contexts/AuthProvider.jsx
@@ -9,7 +9,7 @@ export default function AuthProvider( {children} ) {
     
     const checkLoggedIn = async () => {
         try {
-                const response = await axios.get('http://localhost:5000/auth/loggedIn', 
+                const response = await axios.get('http://localhost:5000/auth/me', 
                 {
                     withCredentials: true
                 }
