19fdae6d87a24631e2120e917d9893570e137178
Stefan Lange <stefanlange@maeve.de>
2024-02-23 11:50:11 +0100

    profile init

---
+++ frontend/src/components/profile/ProfileForm.jsx
@@ -0,0 +1,10 @@
+export default function ProfileForm() {
+    return (
+        <div>
+            <h2>Profile</h2>
+            <form>
+                <p>Profile fields go here</p>
+            </form>
+        </div>
+    );
+}
