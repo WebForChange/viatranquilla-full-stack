624f1a1835e6b257d82f7dc924f337246759babc
Stefan Lange <stefanlange@maeve.de>
2024-02-26 13:57:09 +0100

    finished login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 import {Routes, Route} from "react-router-dom";
 import Userprofile from "./pages/Userprofile";
 import TripDetails from "./pages/TripDetails";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 export default function App() {
   return (
@@ -13,6 +15,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/user" element={<Userprofile />} />
         <Route path="/tripdetails" element={<TripDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   )
