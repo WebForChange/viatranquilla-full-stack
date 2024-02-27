af2b796460b10a341eded63cded69727a3cc9584
Oskar <oskar.radetzki.dev@googlemail.com>
2024-02-27 16:36:34 +0100

    ProfileByUsername change

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -8,10 +8,10 @@ export const getAllUsers = asyncHandler(async (req, res, next) => {
   res.json(user);
 });
 
-export const getProfileDataByID = asyncHandler(async (req, res, next) => {
+export const getProfileDataByUsername = asyncHandler(async (req, res, next) => {
   const username = req.params.username;
 
-  const profile = await Profile.findOne({
+  const profile = await User.findOne({
     username: username,
   });
 
