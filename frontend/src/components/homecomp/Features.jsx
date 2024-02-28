1a4da091c65635e3f1e850cb261f41e8c62a2815
Bepixeled <git@bepixeled.dev>
2024-02-28 10:27:36 +0100

    style changes homepage. logo added. features section workover

--- frontend/src/components/homecomp/Features.jsx
+++ frontend/src/components/homecomp/Features.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
+import React from "react";
+import * as Unicons from "@iconscout/react-unicons";
 
 function Features() {
   return (
-    <div className="features">
-    
-    <div className="flex flex-col mx-auto justify-center items-center">
-    <div className="card lg:card-side border border-solid border-delft_blue-200 shadow-xl w-2/3 my-8">
-      <figure>
-        <img
-          src="https://www.thespruce.com/thmb/i4MqH88_cir5pwUpSD9xrZZBR1c=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Lotusbelle-outback-deluxe-tent-viasmallspaces.about.com-56a888335f9b58b7d0f31ba1.jpg"
-          alt="Tent"
-          className="max-w-[300px] md:max-w-[500px] md:min-h-[300px] hover:scale-105 transition duration-300 ease-in-out transform"
-        />
-      </figure>
-      <div className="card-body glass">
-        <h2 className="card-title text-sage">Glamping!</h2>
-        <p className="text-gray-800">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
-        <div className="card-actions justify-end">
-          <button className="btn bg-cambridge_blue-300 border-none glass hover:bg-cambridge_blue-600">Read more</button>
+    <div className="features p-2">
+      <div className="flex flex-col lg:flex-row mx-auto justify-center items-center gap-4 lg:gap-8 my-8 lg:mb-16">
+
+        <div className="flex flex-col bg-cambridge_blue-200 glass rounded-xl p-4 text-eggshell-600 w-80 lg:w-96 items-center shadow-lg shadow-delft_blue-100">
+          <Unicons.UilCar size={48} className="text-5xl text-sunset mb-4" />
+          <div className="">
+            <p className="text-justify">
+              Plan trips with your friends. No matter if it's a Citytrip,
+              Roadtrip or an unknown adventure. Plan start and end date, routes
+              and possible stopovers. Are you planning on visiting certain
+              places? Let your friends know about all plans. From sightseeing to
+              eating out.
+            </p>
+          </div>
         </div>
+
+        <div className="flex flex-col bg-cambridge_blue-300 glass rounded-xl p-4 text-eggshell-600 w-80 lg:w-96 items-center shadow-lg shadow-delft_blue-100">
+          <Unicons.UilUserArrows size={48} className="text-5xl text-sunset mb-4" />
+          <div className="">
+            <p className="text-justify">
+              Get social and meet new friends. Find people with the same adventurer spirit as you. Share your trips and experiences with others. Get inspired by other people's trips and experiences. Find new friends and travel buddies to explore the world with.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col bg-cambridge_blue-200 glass rounded-xl p-4 text-eggshell-600 w-80 lg:w-96 items-center shadow-lg shadow-delft_blue-100">
+          <Unicons.UilNotes size={48} className="text-5xl text-sunset mb-4" />
+          <div className="">
+            <p className="text-justify">
+              Write down your thoughts and feelings. Share your trips and experiences with others. In your travel log you can write down everything you want to remember. From the best restaurant to the most beautiful sunset. Share your travel log with your friends and family.
+            </p>
+          </div>
+        </div>
+
       </div>
     </div>
-    </div>
-  </div>
-
-  )
+  );
 }
 
-export default Features
+export default Features;
