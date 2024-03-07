fb7b33224dc9ba36255d844550c471f9ea74ff5f
Oskar <oskar.radetzki.dev@googlemail.com>
2024-03-07 11:08:16 +0100

    navigation to message after oauth and some toasts as utils

--- frontend/src/components/shared/OAuthMessage.jsx
+++ frontend/src/components/shared/OAuthMessage.jsx
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 
 export default function OAuthRegister() {
   return (
-    <div className="flex justify-center items-center h-screen w-full  text-burnt_sienna-400 bg-301 bg-contain bg-no-repeat my-8">
+    <div className="flex justify-center items-center h-screen w-full  text-burnt_sienna-400 bg-301 bg-contain">
       <div className=" ">
         <div className="flex flex-col items-center justify-center space-y-8 px-8">
-        <p className="text-eggshell-300 text-xl lg:text-2xl font-semibold text-center">
-        !!! Congratulations !!!<br/><br/><br/>
+        <p className="text-sunset-300 text-xl lg:text-6xl font-bold text-center pb-15">
+        🥳 Congratulations 🥳<br/><br/>
         </p>
-          <p className="text-eggshell-300 text-xl lg:text-2xl font-semibold text-center">
+          <p className="text-sunset-400 text-xl lg:text-2xl font-semibold text-center">
           You have successfully registered with your Google account!<br/><br/>
 
           We have just sent you a confirmation with a specially generated password to your email.<br/><br/>
 
           Also check your spam folder!<br/><br/>
 
-          After the first successful login, we ask you to regenerate your password via your profile.<br/><br/>
+          After the first successful login, we ask you to renew your password via your profile.<br/><br/>
 
           Now we wish you much success and great adventures!<br/><br/>
 
@@ -23,7 +23,7 @@ export default function OAuthRegister() {
           </p>
           <Link
             to="/login"
-            className="text-eggshell-600 text-xl lg:text-2xl font-semibold hover:underline"
+            className="text-sunset-800 text-xl lg:text-4xl font-semibold hover:underline"
           >
             {" "}
             Go to Login{" "}
