7c1cf59310e5d16abaadbfb31a8e9bb628d58224
Stefan Lange <stefanlange@maeve.de>
2024-03-10 15:26:59 +0100

    created item card

--- frontend/src/components/items/ItemCard.jsx
+++ frontend/src/components/items/ItemCard.jsx
@@ -2,7 +2,10 @@ export default function ItemCard({ item }) {
     
     return (
         <div>
-
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+            <p>{item.quantity}</p>
+            <p>{item.category}</p>
         </div>
     )
 }
