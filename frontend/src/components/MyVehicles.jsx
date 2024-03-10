c8165bb73a1e5e435a0e82b3832ef3e944564701
Stefan Lange <stefanlange@maeve.de>
2024-03-10 16:21:40 +0100

    myvehicle component, display and delete

--- frontend/src/components/MyVehicles.jsx
+++ frontend/src/components/MyVehicles.jsx
@@ -1,14 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
 
-export default function MyVehicles({user}) {
+export default function MyVehicles({username}) {
     const [vehicles, setVehicles] = useState([]);
-
+    async function getVehicles() {
+        try {
+            const res = await axios.get(`http://localhost:3000/vehicles/${username}`);
+            setVehicles(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getVehicles();
+    }, []);
+    function deleteVehicle(id) {
+        axios.delete(`http://localhost:3000/vehicles/${id}`)
+            .then(() => {
+                getVehicles();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+    function editVehicle(id) {
+        console.log(id);
+    }
     return (
         <div>
-            <h1>My Vehicles</h1>
+            {!vehicles ? <h1>No Data...</h1> :
+                vehicles.map((vehicle) => {
+                    return (
+                        <div key={vehicle.id}>
+                            <div>
+                                <img src={vehicle.image} alt="vehicle" />
+                            </div>
+                            <div>
+                                <p>{vehicle.make}</p>
+                                <p>{vehicle.model}</p>
+                                <p>{vehicle.year}</p>
+                                <p>{vehicle.color}</p>
+                                <p>{vehicle.seats}</p>
+                                <button onClick={() => deleteVehicle(vehicle.id)}>Delete</button>
+                                <button onClick={() => editVehicle(vehicle.id)}>Edit</button>
+                            </div>
+                        </div>
+                    );
+                })
+            }
         </div>
     );
 }
