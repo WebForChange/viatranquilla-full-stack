6f65814267f0704fe55c1b80d7db48c00feb6cb1
Stefan Lange <stefanlange@maeve.de>
2024-03-10 16:36:19 +0100

    added datacheck to car form for edit

--- frontend/src/components/forms/CarForm.jsx
+++ frontend/src/components/forms/CarForm.jsx
@@ -1,8 +1,9 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
-export default function CarForm() {
+export default function CarForm( {data}) {
     const { user } = useContext(AuthContext);
+    const [edit, setEdit] = useState(false);
     const seats = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
     const [car, setCar] = useState({
         make: "",
@@ -13,21 +14,41 @@ export default function CarForm() {
         image: "",
         creator: user.username,
     });
+    useEffect(() => {
+        if (data) {
+            setEdit(true);
+            setCar(data);
+        }
+    }
+    , []);
+
     const handleChange = (e) => {
         setCar({ ...car, [e.target.name]: e.target.value });
     };
     const handleUpload = async (e) => {
         setCar({ ...car, image: url  });
     };
-    const handleSubmit = async (e) => {
+
+    async function handleSubmit(e) {
         e.preventDefault();
-        try {
-            // await createCar(car);
-            console.log("Car created");
-        } catch (error) {
-            console.log(error);
+        if (edit) {
+            try {
+                const res = await axios.put(`http://localhost:3000/vehicles/${data.id}`, car);
+                console.log(res);
+            }
+            catch (error) {
+                console.log(error);
+            }
+        } else {
+            try {
+                const res = await axios.post("http://localhost:3000/vehicles", car);
+                console.log(res);
+            }
+            catch (error) {
+                console.log(error);
+            }
         }
-    };
+    }
 
     return (
         <div>
@@ -78,7 +99,11 @@ export default function CarForm() {
                     name="image"
                     onChange={handleUpload}
                 />
-                <button onClick={handleSubmit}>Create Car</button>
+                {!edit ? (
+                    <button onClick={handleSubmit}>Add Car</button>
+                ) : (
+                    <button onClick={handleSubmit}>Edit Car</button>
+                )}
             </form>
         </div>
     )
