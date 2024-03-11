18c9c69af75574b4bb7e1885b233397d27a313b7
Stefan Lange <stefanlange@maeve.de>
2024-03-11 09:56:22 +0100

    small issues fixed

--- frontend/src/components/MyVehicles.jsx
+++ frontend/src/components/MyVehicles.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 export default function MyVehicles( {username} ) {
     const navigate = useNavigate();
     const [vehicles, setVehicles] = useState([]);
+    console.log("MyVehicles username: ", username);
     async function getVehicles() {
         try {
             const res = await axios.get(`http://localhost:3000/vehicles/${username}`);
@@ -31,26 +32,26 @@ export default function MyVehicles( {username} ) {
     }
     return (
         <div>
-            {!vehicles ? <h1>No Data...</h1> :
+            {vehicles.length === 0 ? <h2>No Vehicles</h2> :
                 vehicles.map((vehicle) => {
-                    return (
-                        <div key={vehicle.id}>
-                            <div>
-                                <img src={vehicle.image} alt="vehicle" />
-                            </div>
-                            <div>
-                                <p>{vehicle.make}</p>
-                                <p>{vehicle.model}</p>
-                                <p>{vehicle.year}</p>
-                                <p>{vehicle.color}</p>
-                                <p>{vehicle.seats}</p>
-                                <button onClick={() => deleteVehicle(vehicle.id)}>Delete</button>
-                                <button onClick={() => editVehicle(vehicle)}>Edit</button>
-                            </div>
-                        </div>
-                    );
-                })
-            }
+                return (
+                <div key={vehicle.id}>
+                <div>
+                <img src={vehicle.image} alt="vehicle" />
+                </div>
+                <div>
+                    <p>{vehicle.make}</p>
+                    <p>{vehicle.model}</p>
+                    <p>{vehicle.year}</p>
+                    <p>{vehicle.color}</p>
+                    <p>{vehicle.seats}</p>
+                    <button onClick={() => deleteVehicle(vehicle.id)}>Delete</button>
+                    <button onClick={() => editVehicle(vehicle)}>Edit</button>
+                </div>
+            </div>
+        );
+    })
+}
         </div>
     );
 }
