3ab1a57baf47df7cb425085d4816679fabf55283
Stefan Lange <stefanlange@maeve.de>
2024-03-12 21:34:24 +0100

    item form style

--- frontend/src/components/forms/ItemForm.jsx
+++ frontend/src/components/forms/ItemForm.jsx
@@ -56,50 +56,55 @@ export default function ItemForm() {
         }
     };
     return (
-        <div>
-            <div><p>Hello</p></div>
-            <form>
-                <label htmlFor="name">Name</label>
+        <div className="text-eggshell-600 w-full h-screen p-4">
+            <div><h2 className="mx-8 text-center text-1xl lg:text-2xl font-bold mb-4 text-sunset-400">Create a new Item</h2></div>
+            <form className="flex flex-col gap-4 justify-between items-center">
+
+                <label htmlFor="name">Name:</label>
                 <input
                     type="text"
                     name="name"
                     value={item.name}
                     onChange={handleChange}
+                    className="p-1 rounded text-delft_blue-100"
                 />
-                <label htmlFor="description">Description</label>
+                <label htmlFor="description">Description:</label>
                 <input
                     type="text"
                     name="description"
                     value={item.description}
                     onChange={handleChange}
+                    className="p-1 rounded text-delft_blue-100"
                 />
-                <label htmlFor="quantity">Quantity</label>
+                <label htmlFor="quantity">Quantity:</label>
                 <input
                     type="text"
                     name="quantity"
                     value={item.quantity}
                     onChange={handleChange}
+                    className="p-1 rounded text-delft_blue-100"
                 />
-                <label htmlFor="category">Category</label>
+                <label htmlFor="category">Category:</label>
                 <select
                     name="category"
                     value={item.category}
                     onChange={handleChange}
+                    className="p-1 rounded text-delft_blue-100"
                 >
-                    <option value="">Select a category</option>
+                    <option value="" className="p-1 rounded">Select a category</option>
                     {categories.map((category, index) => (
                         <option key={index} value={category}>
                             {category}
                         </option>
                     ))}
                 </select>
-                <label htmlFor="image">Image</label>
+                {/* <label htmlFor="image">Image</label>
                 <input
                     type="file"
                     name="image"
                     onChange={handleUpdate}
-                />
-                {populated ? <button onClick={handleUpdate}>Update</button> : <button onClick={handleSubmit}>Submit</button>}
+                /> */}
+                {populated ? <button onClick={handleUpdate} className="btn bg-cambridge_blue-500 text-delft_blue-100 border-none hover:bg-cambridge_blue-500">Update</button> : <button onClick={handleSubmit} className="btn bg-cambridge_blue-500 text-delft_blue-100 border-none hover:bg-cambridge_blue-500">Submit</button>}
             </form>
         </div>
     )
