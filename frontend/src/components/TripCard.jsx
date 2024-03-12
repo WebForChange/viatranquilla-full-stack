3f91b55a170b78c4c852f674d684944afba9177a
Stefan Lange <stefanlange@maeve.de>
2024-03-12 22:21:14 +0100

    date to ux friendly date

--- frontend/src/components/TripCard.jsx
+++ frontend/src/components/TripCard.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function TripCard({ trip }) {
+  function formatDate(dateString) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  }
   return (
     <div>
       {!trip ? (
@@ -19,7 +23,7 @@ function TripCard({ trip }) {
               <p>{trip.description}</p>
               <p>
                 <em>
-                  {trip.startDate} - {trip.endDate}
+                {formatDate(trip.startDate)} - {formatDate(trip.endDate)}
                 </em>
               </p>
               <div className="card-actions justify-end"></div>
