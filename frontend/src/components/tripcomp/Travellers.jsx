65ee30764321dc94828aa67d1c02a5d73bc305a2
Tonio <tonio.suessdorf@gmail.com>
2024-03-13 11:32:09 +0100

    Fix trip by id to return connections again

--- frontend/src/components/tripcomp/Travellers.jsx
+++ frontend/src/components/tripcomp/Travellers.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 function Travellers({ trip }) {
-
   return (
     <div className="p-8">
       <h2 className="text-2xl font-bold mb-4 text-sunset-400">Travellers</h2>
       <ul>
-        {trip.participants?.map((participant) => (
-          <li key={participant.id} className="flex items-center space-x-2">
+        {trip.participants?.map((participant, index) => (
+          <li key={index} className="flex items-center space-x-2">
             <img
-              src={participants.image || "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"}
+              src={
+                participants.image ||
+                "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              }
               alt="avatar"
               className="w-8 h-8 rounded-full"
             />
